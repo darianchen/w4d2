5130bf20701afb3a3084e9be0cb22086585771f7
darianchen <darianychen@gmail.com>
2022-08-09 11:42:28 -0400

    add manager

--- employee.rb
+++ employee.rb
@@ -1,8 +1,14 @@
 class Employee
-
-    def initialize()
-
+    
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
     end
 
+    def bonus(multiplier)
+        bonus = (@salary) * multiplier
+    end
 
 end
--- manager.rb
+++ manager.rb
@@ -1,6 +1,12 @@
 class Manager < Employee
 
-    
+    def initialize(name,title,salary,boss)
+        super(name,title,salary,boss)
+        @employees = []
+    end
 
+    def bonus(multipler)
+        @employees.inject(0) { |sum ,employee| sum.salary + employee.salary }
+    end
 
 end
