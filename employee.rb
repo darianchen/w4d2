1b3d022e4b2124b94a62c992d57dae31d8e0939e
darianchen <darianychen@gmail.com>
2022-08-09 14:06:38 -0400

    add sub method

--- employee.rb
+++ employee.rb
@@ -1,23 +1,23 @@
+
 class Employee
-    
+    attr_reader :employees, :name
     def initialize(name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+        add_sub if !boss.nil?
     end
 
     def bonus(multiplier)
         bonus = (@salary) * multiplier
     end
 
+    def add_sub
+        @boss.employees << self
+    end
+
+
+
 end
 
-
-ned = Employee.new("Ned", "Founder", 1000000, nil)
-darren = Employee.new("Darren", "TA Manager", 78000, "Ned")
-shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-david = Employee.new("David", "TA", 10000, "Darren")
-
-p david.bonus(3)
-p darren.bonus(4)
--- manager.rb
+++ manager.rb
@@ -1,12 +1,28 @@
+require_relative "employee"
 class Manager < Employee
+    attr_reader :employees, :name
 
     def initialize(name,title,salary,boss)
-        super(name,title,salary,boss)
+        super
         @employees = []
+        add_sub if !boss.nil?
     end
 
     def bonus(multipler)
-        @employees.inject(0) { |sum ,employee| sum.salary + employee.salary }
+        #@employees.inject(0) { |sum ,employee| sum.salary + employee.salary }
     end
 
 end
+
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+
+
+# p ned.bonus(5)
+# p darren.bonus(4)
+# p david.bonus(3)
+
+
