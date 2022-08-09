4094a8210df5872a1f05f843f961092eef551122
“Kyungmin <rudalslee7@gmail.com>
2022-08-09 11:57:55 -0400

    create instances

--- employee.rb
+++ employee.rb
@@ -12,3 +12,12 @@ class Employee
     end
 
 end
+
+
+ned = Employee.new("Ned", "Founder", 1000000, nil)
+darren = Employee.new("Darren", "TA Manager", 78000, "Ned")
+shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+david = Employee.new("David", "TA", 10000, "Darren")
+
+p david.bonus(3)
+p darren.bonus(4)
